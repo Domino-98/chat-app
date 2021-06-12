1e4449083e6ba09922acf2795f74444876fd0ec1
Dominik Buczek <bdominoo@gmail.com>
2021-06-12 15:06:08 +0200

    Create get messages function

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ const path = require('path');
 // Serwowanie plików statycznych
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Tablica zawierająca obiekty wiadomości
+const messages = [
+    {
+        name: "Dominik",
+        message: "Witam"
+    },
+    {
+        name: "Piotr",
+        message: "Cześć"
+    },
+]
+
+// Funkcja pozwala zdefiniować routing dla żądań GET do danego URL.
+app.get('/messages', (req, res) => {
+    res.send(messages);
+});
+
 const port = 3000;
 
 // Uruchomienie aplikacji nasłuchującej na porcie 3000
