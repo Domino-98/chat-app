7dc7df27a8c0766903793ebcecb69e9bf5e39dff
Dominik Buczek <bdominoo@gmail.com>
2021-06-12 23:57:16 +0200

    Create and send POST request

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
 // Serwowanie plików statycznych
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Tablica zawierająca obiekty wiadomości
-const messages = [
-    {
-        name: "Dominik",
-        message: "Witam"
-    },
-    {
-        name: "Piotr",
-        message: "Cześć"
-    },
-]
+app.use(express.json());
 
-// Funkcja pozwala zdefiniować routing dla żądań GET do danego URL.
+// Tablica zawierająca obiekty wiadomości
+const messages = [];
+
+// Funkcja pozwala zdefiniować routing dla żądań GET do danego URL
 app.get('/messages', (req, res) => {
     res.send(messages);
+});
+
+// Obsłużenie żądania POST dla URL localhost:3000/messages. Do tablicy 'messages' jest przesyłany obiekt wiadomości z właściwościami name oraz message
+app.post('/messages', (req, res) => {
+    messages.push(req.body);
+    res.sendStatus(200);
 });
 
 const port = 3000;
